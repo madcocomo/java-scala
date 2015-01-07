c5a1eb6988c8a1bbe572f2e1b33b514d70a85e67
madcocomo <madcocomo@gmail.com>
2015-01-07 14:27:06 +0800

    test 5 r. test case refactoring

--- src/test/kata/tennis/Tennistest.scala
+++ src/test/kata/tennis/Tennistest.scala
@@ -3,8 +3,8 @@ package kata.tennis
 import org.scalatest.{Matchers, FlatSpec}
 
 class Tennistest extends FlatSpec with Matchers{
+  val game = new Game()
   "Scores" should "return score names" in {
-    val game = new Game()
     game.scores(1,0) shouldBe "Fifteen,Love"
     game.scores(2,0) shouldBe "Thirty,Love"
     game.scores(3,0) shouldBe "Forty,Love"
@@ -12,7 +12,6 @@ class Tennistest extends FlatSpec with Matchers{
   }
 
   it should "return winner name" in {
-    val game = new Game()
     game.scores(4,2) shouldBe "Player 1 wins"
   }
 }
