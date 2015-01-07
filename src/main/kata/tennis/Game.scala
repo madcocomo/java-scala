def1b66fce02db81b3de363985e4eb2012910011
madcocomo <madcocomo@gmail.com>
2015-01-07 14:13:07 +0800

    test 3s

--- src/main/kata/tennis/Game.scala
+++ src/main/kata/tennis/Game.scala
@@ -3,6 +3,7 @@ package kata.tennis
 class Game {
   def scores(point1: Int, point2: Int) = {
     if (point1 == 2) "Thirty,Love"
+    else if (point1 == 3) "Forty,Love"
     else "Fifteen,Love"
   }
 
--- src/test/kata/tennis/Tennistest.scala
+++ src/test/kata/tennis/Tennistest.scala
@@ -7,5 +7,6 @@ class Tennistest extends FlatSpec with Matchers{
     val game = new Game()
     game.scores(1,0) shouldBe "Fifteen,Love"
     game.scores(2,0) shouldBe "Thirty,Love"
+    game.scores(3,0) shouldBe "Forty,Love"
   }
 }
