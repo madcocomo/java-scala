7b4cb63ba13b3d9b201bca4bd453f94f43ad1728
madcocomo <madcocomo@gmail.com>
2015-01-07 14:18:41 +0800

    test 4r

--- src/main/kata/tennis/Game.scala
+++ src/main/kata/tennis/Game.scala
@@ -2,15 +2,15 @@ package kata.tennis
 
 class Game {
   def scores(point1: Int, point2: Int) = {
-    val name1 = if (point1 == 2) "Thirty"
-    else if (point1 == 3) "Forty"
-    else if (point1 == 1) "Fifteen"
-    else "Love"
-    val name2 = if (point2 == 2) "Thirty"
-    else if (point2 == 3) "Forty"
-    else if (point2 == 1) "Fifteen"
-    else "Love"
+    val name1 = scoreName(point1)
+    val name2 = scoreName(point2)
     s"$name1,$name2"
   }
 
+  def scoreName(point1: Int): String = {
+    if (point1 == 2) "Thirty"
+    else if (point1 == 3) "Forty"
+    else if (point1 == 1) "Fifteen"
+    else "Love"
+  }
 }
