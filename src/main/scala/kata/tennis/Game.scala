aa9d06b71f2f2bc815dec16af5c982331140607d
madcocomo <madcocomo@gmail.com>
2015-01-08 10:42:34 +0800

    test 6 r. win extractor

--- src/main/scala/kata/tennis/Game.scala
+++ src/main/scala/kata/tennis/Game.scala
@@ -4,13 +4,8 @@ class Game(player1: String, player2: String) {
   def scores(point1: Int, point2: Int) = {
     (point1, point2) match {
       case (Score(name1), Score(name2)) => s"$name1,$name2"
+      case (Win(winner)) => s"$winner wins"
       case _ =>
-          val winner = if (point1 > point2) {
-            player1
-          } else {
-            player2
-          }
-          s"$winner wins"
     }
   }
 
@@ -18,4 +13,17 @@ class Game(player1: String, player2: String) {
     def unapply(point: Int): Option[String] =
       Array("Love", "Fifteen", "Thirty", "Forty").lift(point)
   }
+
+  object Win {
+    def unapply(points:(Int, Int)): Option[String] = {
+      new Some(
+        if (points._1 > points._2) {
+          player1
+        } else {
+          player2
+        }
+      )
+    }
+  }
+
 }
