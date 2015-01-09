d48be3a978d23c28a79bd37d393298899349dcd1
madcocomo <madcocomo@gmail.com>
2015-01-09 14:41:26 +0800

    refactoring. redef extractors

--- src/main/scala/kata/tennis/Game.scala
+++ src/main/scala/kata/tennis/Game.scala
@@ -3,7 +3,7 @@ package kata.tennis
 class Game(player1: String, player2: String) {
   def scores(point1: Int, point2: Int) = {
     (point1, point2) match {
-      case Draw(Deuce()) => "Deuce"
+      case Deuce(_) => "Deuce"
       case Draw(Score(name1), _) => s"$name1 all"
       case (Score(name1), Score(name2)) => s"$name1,$name2"
       case Win(Advantage(player)) => s"$player wins"
@@ -33,15 +33,23 @@ class Game(player1: String, player2: String) {
     }
   }
 
-  object Draw {
-    def unapply(points:(Int, Int)): Option[(Int, Int)] = {
-      if (points._1 == points._2) new Some(points)
-      else None
+  abstract class Draw {
+    def pf: PartialFunction[(Int, Int), (Int,Int)]
+    def unapply(points:(Int, Int)): Option[(Int, Int)] = pf.lift(points)
+//      if (points._1 == points._2) new Some(points)
+//      else None
+
+  }
+  object Draw extends Draw {
+    override def pf: PartialFunction[(Int, Int), (Int, Int)] = {
+      case (p1,p2) if p1 == p2 => (p1, p2)
     }
   }
 
-  object Deuce {
-    def unapply(points:(Int, Int)): Boolean = points._1>=3 && points._2>=3
+  object Deuce extends Draw {
+    override def pf: PartialFunction[(Int, Int), (Int, Int)] = {
+      case (p1,p2) if p1 == p2 && p1 >= 3 => (p1, p2)
+    }
   }
 
 }
