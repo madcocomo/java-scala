9bb70591c7fecf51a2709041f9b5783362791d98
madcocomo <madcocomo@gmail.com>
2015-01-19 10:43:30 +0800

    refactoring. simplify case by inhirete win from Advantage

--- src/main/scala/kata/tennis/Game.scala
+++ src/main/scala/kata/tennis/Game.scala
@@ -6,7 +6,7 @@ class Game(player1: String, player2: String) {
       case Deuce(_) => "Deuce"
       case Draw(Score(name1), _) => s"$name1 all"
       case (Score(name1), Score(name2)) => s"$name1,$name2"
-      case Win(Advantage(player)) => s"$player wins"
+      case Win(player) => s"$player wins"
       case Advantage(player) => s"Advantage $player"
       case _ =>
     }
@@ -25,26 +25,22 @@ class Game(player1: String, player2: String) {
     override def isDefinedAt(x: (Int, Int)): Boolean = x._1 == x._2
     override def apply(v1: (Int, Int)): (Int, Int) = v1
   }
-
   object Draw extends PFExtractor(new DrawPf)
 
-  object Deuce extends PFExtractor (
-    new DrawPf() {
+  class DeucePf extends DrawPf {
       override def isDefinedAt(x: (Int, Int)) = x._1 >= 3 && super.isDefinedAt(x)
-    }
-  )
-
-  class WinPf extends PartialFunction[(Int,Int),(Int,Int)] {
-    override def isDefinedAt(x: (Int, Int)): Boolean = (x._1-x._2).abs > 1
-    override def apply(v1: (Int, Int)): (Int, Int) = v1
   }
-  object Win extends PFExtractor (new WinPf)
+  object Deuce extends PFExtractor (new DeucePf)
 
   class AdvantagePf extends PartialFunction[(Int,Int), String] {
     override def isDefinedAt(x: (Int, Int)): Boolean = x._1 != x._2
     override def apply(v1: (Int, Int)): String = if (v1._1 > v1._2) player1 else player2
   }
-
   object Advantage extends PFExtractor (new AdvantagePf)
+
+  class WinPf extends AdvantagePf{
+    override def isDefinedAt(x: (Int, Int)): Boolean = (x._1-x._2).abs > 1
+  }
+  object Win extends PFExtractor (new WinPf)
 
 }
